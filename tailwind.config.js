931465b490b597656c6c1bb30bb376cc41b2a9f4
azher ali <azhermurad@gmail.com>
2024-06-25 03:49:09 +0000

    add roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Add Roboto as the default sans-serif font
+      },
+    },
   },
   plugins: [],
 }
